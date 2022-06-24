a4be80d66768998d6eb4db310700d2f7d533f472
Ram <ramk97095@gmail.com>
2022-06-24 23:01:35 +0530

    landing page UI

--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,163 @@
+import { Fragment, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
+import { Client } from "twitter-api-sdk";
+
+const client = new Client(process.env.NEXT_PUBLIC_BEARER_TOKEN);
+
+const people = [
+  { name: "Red" },
+  { name: "Cyan" },
+  { name: "Blue" },
+  { name: "White" },
+  { name: "Gray" },
+];
 
 const Home = () => {
+  const [selected, setSelected] = useState(people[0]);
+
+  const getTweetById = async () => {
+    const params = {
+      "tweet.fields": "author_id",
+      "user.fields": "created_at,profile_image_url,name,username"
+    }
+    const tweet = await client.tweets.findTweetById("1538892208237166592", params);
+    console.log(tweet.data.text);
+  };
+
   return (
-    <div className="bg-slate-200 flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="font-Inter bg-[url('/background.svg')] w-full h-screen bg-center bg-cover flex flex-col items-center justify-center py-2">
       <Head>
         <title>Framethis</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Create beatiful {" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-             Tweet Snippets
-          </a>
-        </h1>
+      <main className="prose flex w-full flex-1 flex-col items-center justify-center">
+        <div className="lg:bg-[url('/box.png')] md:lg:bg-[url('/box.png')] bg-center bg-cover w-full h-auto rounded-lg text-center">
+          <h3 className="text-slate-200">Enter Tweet URL</h3>
+          <div className="px-5 lg:px-20 md:px-20">
+            <input
+              id="candidates"
+              type="search"
+              name="search"
+              placeholder="Enter Tweet URL"
+              className="bg-transparent w-full h-10 rounded-lg border p-2 text-white"
+              onKeyDown={getTweetById}
+            />
+          </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+          <div className="lg:mt-20 md:mt-10 mt-5 px-5 lg:px-20 md:px-20 pb-10">
+            <div className="flex flex-col text-white min-h-[12em] w-full bg-gradient-to-r from-indigo-500 to-pink-300 rounded-lg p-5">
+              <div className="flex items-center space-x-2">
+                <Image
+                  src="/author.jpg"
+                  height={70}
+                  width={70}
+                  className="rounded-full"
+                />
+                <div className="flex flex-col text-sm text-left">
+                  <span>Shane Parrish</span>
+                  <span>@ShaneAParrish</span>
+                </div>
+              </div>
+              <div className="text-left">
+                <p>Amateurs have a goal. Professionals have a process.</p>
+              </div>
+            </div>
+          </div>
+        </div>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="space-x-2 lg:space-x-6 md:space-x-6 flex justify-center items-center content-center bg-gray-200 bg-opacity-10 w-full h-auto rounded-lg mt-10 px-1 py-2 lg:px-5 lg:py-5 md:px-5 md:py-5 text-gray-400 text-sm">
+          <div className="w-28">
+            <span>Colors</span>
+            <Listbox value={selected} onChange={setSelected}>
+              <div className="relative mt-1">
+                <Listbox.Button className="relative w-full rounded-lg bg-gray-200 bg-opacity-10 text-white py-2 pl-2 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 sm:text-sm cursor-pointer">
+                  <span className="block truncate">{selected.name}</span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <SelectorIcon
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </Listbox.Button>
+                <Transition
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Listbox.Options className="list-none absolute bottom-full mb-2 mt-1 h-auto w-full overflow-auto rounded-md bg-zinc-800 bg-opacity-90 text-white text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    {people.map((person, personIdx) => (
+                      <Listbox.Option
+                        key={personIdx}
+                        className={({ active }) =>
+                          `relative select-none py-2 px-2 cursor-pointer hover:text-gray-400`
+                        }
+                        value={person}
+                      >
+                        {({ selected }) => (
+                          <>
+                            <span
+                              className={`block truncate ${
+                                selected ? "font-medium" : "font-normal"
+                              }`}
+                            >
+                              {person.name}
+                            </span>
+                            {selected ? (
+                              <span className="absolute inset-y-0 left-0 flex items-center text-amber-600">
+                                <CheckIcon
+                                  className="h-5 w-5"
+                                  aria-hidden="true"
+                                />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </Listbox>
+          </div>
+          <div>
+            <span>Padding</span>
+            <div className="flex space-x-2">
+              <div className="rounded-lg bg-gray-200 bg-opacity-10 w-auto px-2 py-2 cursor-pointer mt-1 hover:text-gray-200">
+                16px
+              </div>
+              <div className="rounded-lg bg-gray-200 bg-opacity-10 w-auto px-2 py-2 cursor-pointer mt-1 hover:text-gray-200">
+                32px
+              </div>
+              <div className="rounded-lg bg-gray-200 bg-opacity-10 w-auto px-2 py-2 cursor-pointer mt-1 hover:text-gray-200">
+                48px
+              </div>
+            </div>
+          </div>
+          <div className="ml-10 mt-6 lg:mt-6 md:mt-6">
+            <button className="bg-[url('/button-bg.svg')] bg-no-repeat bg-top w-full px-4 py-2 rounded-lg text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+              Export
+            </button>
+          </div>
         </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <footer className="text-gray-400 text-xs mt-2 cursor-pointer">
+        <a href="https://writeonce.dev/" target="_blank" className="flex">
+          <h4> Made by </h4>
+          <div className="flex ml-1">
+            <Image
+              src="/icon.png"
+              alt="Writeonce Logo"
+              width={20}
+              height={20}
+            />
+            <h4 className="text-white ml-1">Writeonce</h4>
+          </div>
         </a>
       </footer>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,52 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontSmoothing: ["hover", "focus"],
+    fontSize: {
+      xs: ".75rem",
+      sm: ".875rem",
+      tiny: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+    },
+    fontWeight: {
+      "extra-light": 100,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      bold: 700,
+      "extra-bold": 800,
+      black: 900,
+    },
+    letterSpacing: {
+      tightest: "-.075em",
+      tighter: "-.05em",
+      normal: "0",
+      wider: ".05em",
+      widest: ".25em",
+    },
+    fontFamily: {
+      sans: ["Roboto", "Helvetica", "Arial", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      Inter: ["Inter", "sans-serif"],
+      Montserrat: ["Montserrat", "sans-serif"],
+    },
   },
-  plugins: [],
-}
+
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+  ],
+};

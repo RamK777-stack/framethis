f17b5b09b495cf725277957df60959cd57549d1d
Ram <ramk97095@gmail.com>
2022-06-25 14:10:09 +0530

    Colors added

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,19 @@ import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
 import html2canvas from "html2canvas";
 
-const people = [
-  { name: "Red" },
-  { name: "Cyan" },
-  { name: "Blue" },
-  { name: "White" },
-  { name: "Gray" },
+const colorOptions = [
+  { name: "Candy", colorCode: "bg-gradient-to-r from-indigo-500 to-pink-300" },
+  { name: "Rose", colorCode: "bg-gradient-to-r from-[#f093fb] to-[#f5576c]" },
+  { name: "Blue", colorCode: "bg-gradient-to-r from-[#0093E9] to-[#80D0C7]" },
+  { name: "Cosmic", colorCode: "bg-gradient-to-r from-[#ff00cc] to-[#333399]" },
+  { name: "Love", colorCode: "bg-gradient-to-r from-[#00DBDE] to-[#FC00FF]" },
+  { name: "Milk", colorCode: "bg-white text-gray-900 font-semibold" },
 ];
 
 const Home = () => {
-  const [selected, setSelected] = useState(people[0]);
+  const [selected, setSelected] = useState(colorOptions[0]);
   const [tweetDetails, setTweetDetails] = useState();
+  const [padding, setPadding] = useState("p-4");
   const [tweetURL, setTweetURL] = useState(
     "https://twitter.com/ShaneAParrish/status/1538892208237166592"
   );
@@ -68,7 +70,7 @@ const Home = () => {
   const exportAsImage = async (el, imageFileName) => {
     const canvas = await html2canvas(el);
     const image = canvas.toDataURL("image/jpeg", 1.0);
-    downloadImage(image, imageFileName)
+    downloadImage(image, imageFileName);
   };
 
   const downloadImage = (blob, fileName) => {
@@ -86,7 +88,7 @@ const Home = () => {
   };
 
   return (
-    <div className="font-Inter bg-[url('/background.svg')] w-full h-screen bg-center bg-cover flex flex-col items-center justify-center py-2">
+    <div className="font-Inter bg-[url('/background.svg')] w-full min-h-screen bg-center bg-cover flex flex-col items-center justify-center py-2">
       <Head>
         <title>Framethis</title>
         <link rel="icon" href="/favicon.ico" />
@@ -110,11 +112,15 @@ const Home = () => {
                 e.key === "Enter" && retrieveTweetIDFromURL(e.target.value)
               }
             />
+            <small className="text-slate-200">Copy & paste URL here and hit enter</small>
           </div>
 
-          <div className="lg:mt-20 md:mt-10 mt-5 px-5 lg:px-20 md:px-20 pb-10">
+          <div className="lg:mt-16 md:mt-8 mt-5 px-5 lg:px-20 md:px-20 pb-10">
             <div
-              className="flex flex-col text-white min-h-[12em] w-full bg-gradient-to-r from-indigo-500 to-pink-300 rounded-lg p-5 cursor-pointer"
+              className={`flex flex-col text-white min-h-[12em] w-full ${
+                selected.colorCode || "bg-white"
+              } rounded-lg 
+              ${padding} cursor-pointer`}
               onClick={openTweet}
               ref={exportRef}
             >
@@ -150,7 +156,12 @@ const Home = () => {
             <Listbox value={selected} onChange={setSelected}>
               <div className="relative mt-1">
                 <Listbox.Button className="relative w-full rounded-lg bg-gray-200 bg-opacity-10 text-white py-2 pl-2 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 sm:text-sm cursor-pointer">
-                  <span className="block truncate">{selected.name}</span>
+                  <div className="flex space-x-1">
+                    <div
+                      className={`h-5 w-5 rounded-full ${selected.colorCode}`}
+                    ></div>
+                    <span>{selected.name}</span>
+                  </div>
                   <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                     <SelectorIcon
                       className="h-5 w-5 text-gray-400"
@@ -165,7 +176,7 @@ const Home = () => {
                   leaveTo="opacity-0"
                 >
                   <Listbox.Options className="list-none absolute bottom-full mb-2 mt-1 h-auto w-full overflow-auto rounded-md bg-zinc-800 bg-opacity-90 text-white text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                    {people.map((person, personIdx) => (
+                    {colorOptions.map((person, personIdx) => (
                       <Listbox.Option
                         key={personIdx}
                         className={({ active }) =>
@@ -175,21 +186,20 @@ const Home = () => {
                       >
                         {({ selected }) => (
                           <>
-                            <span
-                              className={`block truncate ${
-                                selected ? "font-medium" : "font-normal"
-                              }`}
-                            >
-                              {person.name}
-                            </span>
                             {selected ? (
-                              <span className="absolute inset-y-0 left-0 flex items-center text-amber-600">
+                              <span className="absolute inset-y-0 right-full flex items-center text-amber-400">
                                 <CheckIcon
                                   className="h-5 w-5"
                                   aria-hidden="true"
                                 />
                               </span>
                             ) : null}
+                            <div className="flex space-x-2">
+                              <div
+                                className={`h-5 w-5 rounded-full ${person.colorCode}`}
+                              ></div>
+                              <span>{person.name}</span>
+                            </div>
                           </>
                         )}
                       </Listbox.Option>
@@ -202,13 +212,28 @@ const Home = () => {
           <div>
             <span>Padding</span>
             <div className="flex space-x-2">
-              <div className="rounded-lg bg-gray-200 bg-opacity-10 w-auto px-2 py-2 cursor-pointer mt-1 hover:text-gray-200">
+              <div
+                onClick={() => {
+                  setPadding("p-4");
+                }}
+                className="rounded-lg bg-gray-200 bg-opacity-10 w-auto px-2 py-2 cursor-pointer mt-1 hover:text-gray-200"
+              >
                 16px
               </div>
-              <div className="rounded-lg bg-gray-200 bg-opacity-10 w-auto px-2 py-2 cursor-pointer mt-1 hover:text-gray-200">
+              <div
+                onClick={() => {
+                  setPadding("p-8");
+                }}
+                className="rounded-lg bg-gray-200 bg-opacity-10 w-auto px-2 py-2 cursor-pointer mt-1 hover:text-gray-200"
+              >
                 32px
               </div>
-              <div className="rounded-lg bg-gray-200 bg-opacity-10 w-auto px-2 py-2 cursor-pointer mt-1 hover:text-gray-200">
+              <div
+                onClick={() => {
+                  setPadding("p-12");
+                }}
+                className="rounded-lg bg-gray-200 bg-opacity-10 w-auto px-2 py-2 cursor-pointer mt-1 hover:text-gray-200"
+              >
                 48px
               </div>
             </div>
